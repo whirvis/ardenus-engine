056caf73b1b4b5a2411db3cfb6e46ef351be3f69
Trent Summerlin <whirvis@ardenus.org>
2020-09-22 17:09:55 -0500

    Create ArdenusManifest.java

---
+++ src/main/java/org/ardenus/engine/ArdenusManifest.java
@@ -0,0 +1,15 @@
+package org.ardenus.engine;
+
+import java.util.jar.Manifest;
+
+/**
+ * The reader for the Ardenus Engine manifest file.
+ * 
+ * @author Trent Summerlin
+ * @since Ardenus Engine v0.0.1-SNAPSHOT
+ */
+public class ArdenusManifest extends Manifest {
+
+	
+	
+}
