f1cf9223cbd70953ff96887610f456e657169277
Trent Summerlin <whirvis@ardenus.org>
2020-09-22 17:35:26 -0500

    Add requireMainAttributes(Name...) to ArdenusManifest.java
    This method will be used to ensure that any attributes that are required for a
    game to run under the Ardenus Engine are present within a manifest. Curretly,
    the ArdenusManifest only requires that the "Main" attribute be present.

--- src/main/java/org/ardenus/engine/ArdenusManifest.java
+++ src/main/java/org/ardenus/engine/ArdenusManifest.java
@@ -14,7 +14,6 @@ import java.util.jar.Manifest;
 public class ArdenusManifest extends Manifest {
 
 	public static final Name MAIN = new Name("Main");
-	private static final Name[] REQUIRED = new Name[] { MAIN };
 
 	/**
 	 * Loads an Ardenus Engine manifest from an input stream.
@@ -26,6 +25,66 @@ public class ArdenusManifest extends Manifest {
 	 */
 	public ArdenusManifest(InputStream in) throws IOException {
 		super(in);
+		this.requireMainAttributes(MAIN);
+	}
+
+	/**
+	 * Ensures that a set of main attributes are present within this manifest.
+	 * If any are missing, an <code>IOException</code> will be thrown.
+	 * 
+	 * @param names
+	 *            the names of the main attributes.
+	 * @throws IOException
+	 *             if a main attribute under one of the names of
+	 *             <code>names</code> is not present within this manifest.
+	 */
+	private void requireMainAttributes(Name... names) throws IOException {
+		if (names != null) {
+			for (Name name : names) {
+				if (name != null && !this.hasMainAttribute(name)) {
+					throw new IOException("Missing required main attribute "
+							+ name.toString());
+				}
+			}
+		}
+	}
+
+	/**
+	 * Checks and returns if a main attribute with a given name exists.
+	 * 
+	 * @param name
+	 *            the attribute name.
+	 * @return <code>true</code> if an attribute under <code>name</code> exists,
+	 *         <code>false</code> otherwise.
+	 */
+	public boolean hasMainAttribute(Name name) {
+		return this.getMainAttributes().get(name) != null;
+	}
+
+	/**
+	 * Returns the value of a main attribute with the given name.
+	 * 
+	 * @param name
+	 *            the attribute name.
+	 * @return the value of the attribute, trimmed by {@link String#trim()},
+	 *         <code>null</code> if it does not exist.
+	 */
+	public String getMainAttribute(Name name) {
+		String value = (String) this.getMainAttributes().get(name);
+		return value != null ? value.trim() : null;
+	}
+
+	/**
+	 * Returns the value of a main attribute with the given name.
+	 * 
+	 * @param name
+	 *            the attribute name.
+	 * @return the value of the attribute, trimmed by {@link String#trim()},
+	 *         <code>fallback</code> (untrimmed) if it does not exist.
+	 */
+	public String getMainAttribute(Name name, String fallback) {
+		String value = this.getMainAttribute(name);
+		return value != null ? value : fallback;
 	}
 
 }
