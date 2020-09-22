2a16edd6ad4046609259babdda2d84f672c34e5c
Trent Summerlin <whirvis@ardenus.org>
2020-09-22 17:59:25 -0500

    Add engine info constants to Ardenus.java

--- src/main/java/org/ardenus/engine/Ardenus.java
+++ src/main/java/org/ardenus/engine/Ardenus.java
@@ -10,6 +10,11 @@ import java.io.File;
  */
 public class Ardenus {
 
+	public static final String ENGINE_NAME = "Ardenus Engine";
+	public static final String ENGINE_VERSION = "v0.0.1 ALPHA";
+	public static final long ENGINE_VERSION_ID = 0x0000000000000001L;
+	public static final String ENGINE_VERSION_CODENAME = "Jumpscare";
+	
 	/**
 	 * The folder in which the Ardenus Engine stores things such as
 	 * configuration files.
