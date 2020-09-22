bc4d9e512a362c63a4f37c755b1e69977c07051f
Trent Summerlin <whirvis@ardenus.org>
2020-09-22 17:28:14 -0500

    Add base methods to ArdenusManifest.java
    The purpose of this class is to provide a way for the game to specify how it
    should be run by the engine. These base methods provide the functionality to
    implement attributes as they are needed or made available in the coming future.

--- src/main/java/org/ardenus/engine/ArdenusManifest.java
+++ src/main/java/org/ardenus/engine/ArdenusManifest.java
@@ -1,5 +1,8 @@
 package org.ardenus.engine;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.jar.Attributes.Name;
 import java.util.jar.Manifest;
 
 /**
@@ -10,6 +13,19 @@ import java.util.jar.Manifest;
  */
 public class ArdenusManifest extends Manifest {
 
-	
-	
+	public static final Name MAIN = new Name("Main");
+	private static final Name[] REQUIRED = new Name[] { MAIN };
+
+	/**
+	 * Loads an Ardenus Engine manifest from an input stream.
+	 * 
+	 * @param in
+	 *            the input stream.
+	 * @throws IOException
+	 *             if an I/O error occurs.
+	 */
+	public ArdenusManifest(InputStream in) throws IOException {
+		super(in);
+	}
+
 }
