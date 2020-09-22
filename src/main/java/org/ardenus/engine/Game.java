b50a8d11a9a0444eb408b714d591300eb169416b
Trent Summerlin <whirvis@ardenus.org>
2020-09-22 17:06:44 -0500

    Create Game.java

---
+++ src/main/java/org/ardenus/engine/Game.java
@@ -0,0 +1,11 @@
+package org.ardenus.engine;
+
+/**
+ * A game running through the Ardenus Engine.
+ * 
+ * @author Trent Summerlin
+ * @since Ardenus Engine v0.0.1-SNAPSHOT
+ */
+public class Game {
+
+}
