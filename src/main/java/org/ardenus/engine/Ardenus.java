65b61b3bda399e0b7e4c4648df5d0191cbfb33c0
Trent Summerlin <whirvis@ardenus.org>
2020-09-22 18:44:04 -0500

    Add main(), start() to Ardenus.java
    This method currently doesn't do anything except for check whether or not the
    development mode is enabled via the startup arguments. The main() and start()
    functions will be updated appropiately as the engine is developed.

--- src/main/java/org/ardenus/engine/Ardenus.java
+++ src/main/java/org/ardenus/engine/Ardenus.java
@@ -2,8 +2,13 @@ package org.ardenus.engine;
 
 import java.io.File;
 
+import com.whirvex.cmd.Option;
+import com.whirvex.cmd.args.Args;
+
 /**
  * The Ardenus game engine.
+ * <p>
+ * TODO: Go into depth what this class is for.
  * 
  * @author Trent Summerlin
  * @since Ardenus Engine v0.0.1-SNAPSHOT
@@ -14,15 +19,65 @@ public class Ardenus {
 	public static final String ENGINE_VERSION = "v0.0.1 ALPHA";
 	public static final long ENGINE_VERSION_ID = 0x0000000000000001L;
 	public static final String ENGINE_VERSION_CODENAME = "Jumpscare";
-	
+
 	/**
 	 * The folder in which the Ardenus Engine stores things such as
 	 * configuration files.
 	 */
 	public static final File ENGINE_ROOT = new File("./ardenus/");
-	
+
+	private static boolean started;
+	private static boolean devmode;
+
 	private Ardenus() {
 		// Static class
 	}
-	
+
+	/**
+	 * Returns whether or not the engine is running in development mode.
+	 * <p>
+	 * TODO: Define what development mode being enabled means.
+	 * <p>
+	 * Development mode is enabled by specifying the <code>--devmode</code>
+	 * option at startup via the JVM program arguments.
+	 * 
+	 * @return <code>true</code> if the engine is running in development mode,
+	 *         <code>false</code> otherwise.
+	 */
+	public static boolean devmode() {
+		return devmode;
+	}
+
+	/**
+	 * Starts the Ardenus Engine.
+	 * <p>
+	 * TODO: Go into depth what this method does.
+	 * 
+	 * @param args
+	 *            the engine boot arguments.
+	 * @throws IllegalStateException
+	 *             if the engine has already been started.
+	 */
+	public static synchronized void start(Args args) {
+		if (started == true) {
+			throw new IllegalStateException("engine already started");
+		}
+		devmode = args.has("devmode");
+		started = true;
+	}
+
+	/**
+	 * Java program entry point.
+	 * 
+	 * @param args
+	 *            the program arguments.
+	 */
+	public static void main(String[] args) {
+		Option help = Option.opt().key('h', "help").value(false)
+				.desc("Displays engine info and start options").build();
+		Option devmode = Option.opt().key("devmode").value(false)
+				.desc("If the engine should run in development mode").build();
+		start(Args.parse(args, help, devmode));
+	}
+
 }
