5a454019ada97f407c77ac80e2d0c065dfa28c80
Trent Summerlin <whirvis@ardenus.org>
2020-09-22 22:23:05 -0500

    Add constructor for Game.java

--- src/main/java/org/ardenus/engine/Game.java
+++ src/main/java/org/ardenus/engine/Game.java
@@ -1,5 +1,10 @@
 package org.ardenus.engine;
 
+import java.util.Objects;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * A game running through the Ardenus Engine.
  * 
@@ -7,5 +12,25 @@ package org.ardenus.engine;
  * @since Ardenus Engine v0.0.1-SNAPSHOT
  */
 public class Game {
+
+	private Logger logger;
+	
+	/**
+	 * The games title. By default, this is what the window title is set to.
+	 */
+	public final String title;
+
+	/**
+	 * Constructs a new <code>Game</code>.
+	 * 
+	 * @param title
+	 *            the game title.
+	 * @throws NullPointerException
+	 *             if <code>title</code> is <code>null</code>.
+	 */
+	protected Game(String title) {
+		this.logger = LogManager.getLogger("game");
+		this.title = Objects.requireNonNull(title, "title cannot be null");
+	}
 
 }
