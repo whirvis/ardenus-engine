8b34206c5e3afccf74dc9fb85821fcfa1e6d65bd
Trent Summerlin <whirvis@ardenus.org>
2021-07-24 07:40:04 -0500

    Add value() to BeResponsible annotation

--- src/main/java/org/ardenus/engine/util/BeResponsible.java
+++ src/main/java/org/ardenus/engine/util/BeResponsible.java
@@ -7,8 +7,15 @@ import java.lang.annotation.Documented;
  * <b>performs no checks</b> to ensure validity of context, state, parameters,
  * etc. The reasons for this will probably vary. However, the usual reason is
  * for a speed advantage (e.g., graphics rendering, audio processing, etc.)
+ * <p>
+ * A value for this annotation can be specified to explain exactly the
+ * programmer must "be responsible" when calling the annotated code. By default,
+ * {@link #value()} defaults to {@code "performance"}.
  */
 @Documented
 public @interface BeResponsible {
+
 	/* Razon? Gato sin lag. */
+	String value() default "performance";
+
 }
