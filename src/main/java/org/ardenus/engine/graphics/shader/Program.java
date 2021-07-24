65b5b8d5b6e511588e8af8dffed6616800a432bc
Trent Summerlin <whirvis@ardenus.org>
2021-07-24 07:33:38 -0500

    Add basic OpenGL Shader & Program classes
    No testing has been done on these, but they already come with some
    documentation filled out. They're pretty bare bones for now. More
    features will be added as they are needed. I intend to first implement
    basic shapes, 2D image rendering, and text rendering.

---
+++ src/main/java/org/ardenus/engine/graphics/shader/Program.java
@@ -0,0 +1,286 @@
+package org.ardenus.engine.graphics.shader;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+
+import org.ardenus.engine.graphics.GraphicsException;
+import org.ardenus.engine.util.BeResponsible;
+
+/**
+ * An OpenGL program made up of shaders.
+ * 
+ * @see Shader
+ */
+public class Program implements Closeable {
+
+	/**
+	 * Sets a uniform for the currently installed program.
+	 * <p>
+	 * This is a cheap operation at the cost of parameter checks.<br>
+	 * If an invalid uniform location is specified, tough!
+	 * 
+	 * @param location
+	 *            the uniform location.
+	 * @param value
+	 *            the new uniform value.
+	 * @see #getUniformLocation(String)
+	 * @see #use()
+	 */
+	@BeResponsible
+	public static void setUniform(int location, int value) {
+		glUniform1i(location, value);
+	}
+
+	private final int h_glProgram;
+	private final Map<Integer, Shader> attached;
+
+	private boolean linked;
+	private boolean closed;
+
+	/**
+	 * Constructs a new {@code Program} and generates an OpenGL program to
+	 * manipulate and send instructions to.
+	 * 
+	 * @throws GraphicsException
+	 *             if the program could not be created.
+	 */
+	public Program() {
+		this.h_glProgram = glCreateProgram();
+		if (h_glProgram == GL_NONE) {
+			throw new GraphicsException("failed to create program");
+		}
+		this.attached = new HashMap<>();
+	}
+
+	private void requireAttached(int... glShaderTypes) {
+		for (int glShaderType : glShaderTypes) {
+			if (!attached.containsKey(glShaderType)) {
+				throw new IllegalStateException("missing "
+						+ Shader.getShaderTypeName(glShaderType) + " shader");
+			}
+		}
+	}
+
+	private void requireUnlinked() {
+		if (linked == true) {
+			throw new IllegalStateException("program already linked");
+		}
+	}
+
+	private void requireLinked() {
+		if (linked == false) {
+			throw new IllegalStateException("program not linked");
+		}
+	}
+
+	/**
+	 * Attaches a shader to the program.
+	 * <p>
+	 * Once a shader has been attached to a program, it cannot be detached.
+	 * After all desired shaders have been linked to the program, the program
+	 * must be linked via {@link #link()}.
+	 * 
+	 * @return this program.
+	 * @param shader
+	 *            the shader to attach.
+	 * @throws NullPointerException
+	 *             if {@code shader} is {@code null}.
+	 * @throws IllegalStateException
+	 *             if this program is already linked; if {@code shader} is not
+	 *             compiled or already attached to another shader.
+	 */
+	public Program attach(Shader shader) {
+		Objects.requireNonNull(shader, "shader");
+		if (shader.program == this) {
+			return this;
+		}
+
+		this.requireUnlinked();
+		shader.requireCompiled();
+		shader.requireNotAttached();
+
+		glAttachShader(h_glProgram, shader.h_glShader);
+		attached.put(shader.glShaderType, shader);
+		shader.program = this;
+		return this;
+	}
+
+	/**
+	 * Attaches the given shaders to the program.
+	 * <p>
+	 * Once a shader has been attached to a program, it cannot be detached.
+	 * After all desired shaders have been linked to the program, the program
+	 * must be linked via {@link #link()}.
+	 * 
+	 * @param shaders
+	 *            the shaders to attach.
+	 * @return this program.
+	 * @throws NullPointerException
+	 *             if {@code shaders} or one of its values are {@code null}.
+	 * @throws IllegalStateException
+	 *             if this program is already linked; if a given shader is not
+	 *             compiled or already attached to another shader.
+	 */
+	public Program attach(Shader... shaders) {
+		Objects.requireNonNull(shaders, "shaders");
+		for (Shader shader : shaders) {
+			this.attach(shader);
+		}
+		return this;
+	}
+
+	/**
+	 * Links the program to OpenGL.
+	 * <p>
+	 * After linking is successfully completed, all previously attached shaders
+	 * will be deleted. Once a program has been linked, it must be installed
+	 * into OpenGL for rendering via {@link #use()}.
+	 * 
+	 * @throws IllegalStateException
+	 *             if the program is already linked; if there is no attached
+	 *             {@code GL_VERTEX_SHADER} or {@code GL_FRAGMENT_SHADER}.
+	 * @throws GraphicsException
+	 *             if the program fails to link.
+	 */
+	public void link() {
+		this.requireUnlinked();
+		this.requireAttached(GL_VERTEX_SHADER);
+
+		glLinkProgram(h_glProgram);
+		if (glGetProgrami(h_glProgram, GL_LINK_STATUS) != GL_TRUE) {
+			String programLog = glGetProgramInfoLog(h_glProgram);
+			throw new GraphicsException(programLog);
+		}
+
+		/*
+		 * Once a program has been linked in OpenGL, the shaders that are
+		 * attached to it are no longer needed. OpenGL shaders can technically
+		 * be used with other programs after this. However, to my knowledge,
+		 * this is a rare occasion. I've personally never seen it done, or even
+		 * heard of. So, I've decided just detach and close the shaders here.
+		 */
+		Iterator<Shader> attachedI = attached.values().iterator();
+		while (attachedI.hasNext()) {
+			Shader shader = attachedI.next();
+			glDetachShader(h_glProgram, shader.h_glShader);
+			shader.destroy();
+			attachedI.remove();
+		}
+
+		this.linked = true;
+	}
+
+	/**
+	 * Queries OpenGL for the location of a uniform based on its name.
+	 * <p>
+	 * This is required for safe code that will consistently update the correct
+	 * uniform. However, this is a costly operation. It is recommended to cache
+	 * the values returned from this method into a variable somewhere for later.
+	 * A clean and easy way to do this would be to make a class extending
+	 * {@code Program}, override {@link #link()}, and fetch the uniforms there:
+	 * 
+	 * <pre>
+	 * public class BubblesProgram extends Program {
+	 *     
+	 *     private int u_bubbleX;
+	 *     private int u_bubblyY;
+	 *     private int u_bubbleColor;
+	 *     
+	 *     &commat;Override
+	 *     public void link() {
+	 *         super.link();
+	 *         
+	 *         this.u_bubbleX = this.getUniformLocation("bubble_x");
+	 *         this.u_bubbleY = this.getUniformLocation("bubble_y");
+	 *         this.u_bubbleColor = this.getUniformLocation("bubble_color");
+	 *     }
+	 *     
+	 * }
+	 * </pre>
+	 * 
+	 * @param name
+	 *            the uniform name.
+	 * @return the uniform location.
+	 * @throws NullPointerException
+	 *             if {@code name} is {@code null}.
+	 * @throws IllegalArgumentException
+	 *             if no such uniform {@code name} exists in this program.
+	 * @see #setUniform(int, int)
+	 */
+	public int getUniformLocation(String name) {
+		/*
+		 * Usually for get methods, I would just have this return something like
+		 * -1. However, I've come to think that is a bit silly. From now on, I
+		 * will write methods to throw exceptions for any value that doesn't
+		 * make sense in that context. So... why would you pass null to this?
+		 */
+		Objects.requireNonNull(name, "name");
+		this.requireLinked();
+
+		int location = glGetUniformLocation(h_glProgram, name);
+		if (location < 0) {
+			throw new IllegalArgumentException(
+					"no such uniform \"" + name + "\"");
+		}
+		return location;
+	}
+
+	/**
+	 * Installs the program for use into OpenGL.
+	 * <p>
+	 * It is possible to use multiple render programs in OpenGL. This is an
+	 * expensive operations. As such, it is recommended that shader programs
+	 * performing similar operations be merged into what's called an <a href=
+	 * "http://hacksoflife.blogspot.com/2009/02/uber-shaders-evolution-or-optimization.html">
+	 * Uber Shader</a>. However, this should only be done branching can be
+	 * avoided. If branching is used, <a href=
+	 * "https://gamedev.stackexchange.com/questions/59476/using-two-shaders-instead-of-one-with-if-statements">the
+	 * effects are either negated or worsened</a>.</i>
+	 * <p>
+	 * An example of branching:
+	 * 
+	 * <pre>
+	 * #define DRAW_RECT 0
+	 * #define DRAW_IMG  1
+	 * 
+	 * uniform int task;
+	 * 
+	 * void drawRect();
+	 * void drawImg();
+	 * 
+	 * void main(void) {
+	 *     /&ast; this destroys performance! &ast;/
+	 *     /&ast; no, using a switch won't fix this! &ast;/
+	 *     if (task == DRAW_RECT) {
+	 *         drawRect();
+	 *     } else if (task == DRAW_IMG) {
+	 *         drawImg();
+	 *     }
+	 * }
+	 * </pre>
+	 * 
+	 * @throws IllegalStateException
+	 *             if the program is not linked.
+	 */
+	public void use() {
+		this.requireLinked();
+		glUseProgram(h_glProgram);
+	}
+
+	@Override
+	public void close() throws IOException {
+		if (this.closed == true) {
+			return;
+		}
+		glDeleteProgram(h_glProgram);
+		this.closed = true;
+	}
+
+}
---
+++ src/main/java/org/ardenus/engine/graphics/shader/Shader.java
@@ -0,0 +1,233 @@
+package org.ardenus.engine.graphics.shader;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL32.*;
+import static org.lwjgl.opengl.GL40.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+import org.apache.commons.io.IOUtils;
+import org.ardenus.engine.graphics.GraphicsException;
+
+/**
+ * A shader for an OpenGL program.
+ * 
+ * @see Program
+ */
+public class Shader {
+
+	protected static String getShaderTypeName(int glShaderType) {
+		switch (glShaderType) {
+			case GL_VERTEX_SHADER:
+				return "GL_VERTEX_SHADER";
+			case GL_FRAGMENT_SHADER:
+				return "GL_FRAGMENT_SHADER";
+			case GL_GEOMETRY_SHADER:
+				return "GL_GEOMETRY_SHADER";
+			case GL_TESS_CONTROL_SHADER:
+				return "GL_TESS_CONTROL_SHADER";
+			case GL_TESS_EVALUATION_SHADER:
+				return "GL_TESS_EVALUATION_SHADER";
+			default:
+				return null;
+		}
+	}
+
+	private static int requireShaderType(int glShaderType) {
+		switch (glShaderType) {
+			case GL_VERTEX_SHADER:
+			case GL_FRAGMENT_SHADER:
+			case GL_GEOMETRY_SHADER:
+			case GL_TESS_CONTROL_SHADER:
+			case GL_TESS_EVALUATION_SHADER:
+				return glShaderType;
+			default:
+				throw new IllegalArgumentException("not an OpenGL shader type");
+		}
+	}
+
+	protected final int glShaderType;
+	protected final int h_glShader;
+	
+	private boolean hasSource;
+	private boolean compiled;
+	protected Program program;
+	private boolean destroyed;
+
+	/**
+	 * Constructs a new {@code Shader} and generates an OpenGL shader to
+	 * manipulate and send instructions to.
+	 * 
+	 * @param glShaderType
+	 *            the OpenGL shader type.
+	 * @throws IllegalArgumentException
+	 *             if {@code glShaderType} is not an OpenGL shader type.
+	 * @throws GraphicsException
+	 *             if the shader could not be created.
+	 */
+	public Shader(int glShaderType) {
+		this.glShaderType = requireShaderType(glShaderType);
+		this.h_glShader = glCreateShader(glShaderType);
+		if (h_glShader == GL_NONE) {
+			throw new GraphicsException("failed to create shader");
+		}
+	}
+
+	protected void requireIntact() {
+		if (destroyed == true) {
+			throw new IllegalStateException("shader deleted");
+		}
+	}
+
+	protected void requireHasSource() {
+		if (hasSource == false) {
+			throw new IllegalStateException("no source for shader");
+		}
+	}
+
+	protected void requireUncompiled() {
+		if (compiled == true) {
+			throw new IllegalStateException("shader already compiled");
+		}
+	}
+
+	protected void requireCompiled() {
+		if (compiled == false) {
+			throw new IllegalStateException("shader not compiled");
+		}
+	}
+
+	protected void requireNotAttached() {
+		if (program != null) {
+			throw new IllegalStateException(
+					"shader already attached to program");
+		}
+	}
+
+	/**
+	 * Updates the shader source.
+	 * <p>
+	 * Once the source code for a shader has been set, it must be compiled via
+	 * the {@link #compile()} method.
+	 * 
+	 * @param src
+	 *            the shader source code.
+	 * @return this shader.
+	 * @throws NullPointerException
+	 *             if {@code src} is {@code null}.
+	 * @throws IllegalStateException
+	 *             if the shader is closed or has already been compiled.
+	 */
+	public Shader setSource(String src) {
+		Objects.requireNonNull(src, "src");
+		this.requireIntact();
+		this.requireUncompiled();
+		glShaderSource(h_glShader, src);
+		this.hasSource = true;
+		return this;
+	}
+
+	/**
+	 * Updates the shader source.
+	 * <p>
+	 * Once the source code for a shader has been set, it must be compiled via
+	 * the {@link #compile()} method.
+	 * <p>
+	 * This function is a shorthand {@link #setSource(String)}, with the
+	 * contents of {@code in} being read into a string and passed as the value
+	 * for {@code src}.
+	 * 
+	 * @param in
+	 *            the stream whose contents to use as the shader source.
+	 * @return this shader.
+	 * @throws NullPointerException
+	 *             if {@code in} is {@code null}.
+	 * @throws IllegalStateException
+	 *             if the shader is closed or has already been compiled.
+	 * @throws IOException
+	 *             if an I/O error occurs.
+	 */
+	public Shader setSource(InputStream in) throws IOException {
+		Objects.requireNonNull(in, "in");
+		String src = IOUtils.toString(in, (Charset) null);
+		return this.setSource(src);
+	}
+
+	/**
+	 * Updates the shader source.
+	 * <p>
+	 * Once the source code for a shader has been set, it must be compiled via
+	 * the {@link #compile()} method.
+	 * <p>
+	 * This function is a shorthand {@link #setSource(InputStream)}, with the
+	 * contents of {@code file} being wrapped into a {@link FileInputStream} and
+	 * passed as the value for {@code in}.
+	 * 
+	 * @param file
+	 *            the file whose contents to use as the shader source.
+	 * @return this shader.
+	 * @throws NullPointerException
+	 *             if {@code file} is {@code null}.
+	 * @throws IllegalStateException
+	 *             if the shader is closed or has already been compiled.
+	 * @throws IOException
+	 *             if an I/O error occurs.
+	 */
+	public Shader setSource(File file) throws IOException {
+		Objects.requireNonNull(file, "file");
+		return this.setSource(new FileInputStream(file));
+	}
+
+	/**
+	 * Compiles the shader.
+	 * <p>
+	 * Once the shader has been compiled, it must be attached to a program via
+	 * the {@link Program#attach(Shader)} method. After the program has been
+	 * linked, this shader will be automatically closed. This will prevent the
+	 * shader from being used with any other programs.
+	 * 
+	 * @return this shader.
+	 * @throws IllegalStateException
+	 *             if the shader is closed, has no source code, or has already
+	 *             been compiled.
+	 * @throws GraphicsException
+	 *             if shader compilation fails.
+	 */
+	public Shader compile() {
+		this.requireIntact();
+		this.requireHasSource();
+		this.requireUncompiled();
+
+		glCompileShader(h_glShader);
+		if (glGetShaderi(h_glShader, GL_COMPILE_STATUS) != GL_TRUE) {
+			String shaderLog = glGetShaderInfoLog(h_glShader);
+			throw new GraphicsException(shaderLog);
+		}
+		this.compiled = true;
+		return this;
+	}
+
+	/**
+	 * Deletes the shader from OpenGL.
+	 * <p>
+	 * Usually, a method like this would be an implementation of Java's
+	 * {@link Closeable} interface. However, this method should only be called
+	 * after this shader has been compiled and attached to a linked OpenGL
+	 * program. Implementing the interface would prevent us from having this
+	 * marked as {@code protected} so only {@link Program} could call it.
+	 */
+	protected void destroy() {
+		if (destroyed == true) {
+			return;
+		}
+		glDeleteShader(h_glShader);
+		this.destroyed = true;
+	}
+
+}
