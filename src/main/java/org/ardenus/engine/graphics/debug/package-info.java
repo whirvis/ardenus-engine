fd2bf9e7711355ccf8189798391746c8923d5cea
Trent Summerlin <whirvis@ardenus.org>
2021-07-23 21:47:37 -0500

    Remove package-info from graphics.debug package

--- src/main/java/org/ardenus/engine/graphics/debug/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The debug componenents of the graphics system, these are used to log outputs
- * from the OpenGL API to the console.
- * 
- * @see org.ardenus.engine.graphics.debug.GLMessageSource DebugSource
- * @see org.ardenus.engine.graphics.debug.GLMessageType DebugType
- * @see org.ardenus.engine.graphics.debug.GLMessageSeverity DebugSeverity
- */
-package org.ardenus.engine.graphics.debug;
